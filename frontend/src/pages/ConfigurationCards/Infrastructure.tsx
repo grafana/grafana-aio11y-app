73a54d0f1bc17c8826fea810b03b275462babcf7
Nicholas Landreville <n1ck@hey.com>
2024-07-15 13:48:10 -0500

    add accesspolicytoken

--- frontend/src/pages/ConfigurationCards/Infrastructure.tsx
+++ frontend/src/pages/ConfigurationCards/Infrastructure.tsx
@@ -14,23 +14,15 @@ export function Infrastructure() {
           <h2>Install Grafana Agent In Kubernetes</h2>
           <p>The Grafana agent collects observability data and sends it to Grafana Cloud. Once the agent is deployed to your hosts, it collects and sends Prometheus-style metrics and log data using a pared-down Prometheus collector.</p>
           <p>Run this command to install and run Grafana Agent in Kubernetes</p>
-
           <pre>{dcgmSnippet()}</pre>
-
           <h2>Install DCGM Exporter </h2>
-
           <p>The NVIDIA DCGM Exporter fetches metrics from GPUs and exposes them for collection. It&apos;s crucial for monitoring the performance and health of your GPUs within Kubernetes.</p>
-
           <pre>{installDCGMExporter()}</pre>
-
           <h2>Prepare your agent config file</h2>
-
           <p>Below `metrics.configs.scrape_configs`, insert the following lines: </p>
-
           <pre>
             {prepareAgentConfig()}
           </pre>
-
           <h2>Install Dashboards:</h2>
           <p>Authenticate: <input type="text" onChange={(e) => setInput(e.target.value)} /></p>
           <InstallDashboard filePath={`https://raw.githubusercontent.com/grafana/hackathon-2024-03-tame-the-beast/main/gtm-aiobservability-app/src/instructions/infrastructure/dashboard.json?token=${input}`} />
